07b3d8dba486f95c7ddbc2710539971f9de1307d
CRA Keishi Kawada <keishi.kawada@craftsman-software.com>
2023-10-20 13:21:19 +0900

    chore: more efficiently development

--- development/packages/nextjs-watch-html/app/layout.tsx
+++ development/packages/nextjs-watch-html/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useBrowserEventSpans } from "@react-hook-observability/hooks";
+import { useBrowserEventSpans } from "#hooks";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
